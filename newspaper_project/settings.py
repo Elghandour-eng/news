f99d8f2b21d4b9530a0c2df95422c19aaacfcf8d
Elghandour-eng <63301279+Elghandour-eng@users.noreply.github.com>
2020-08-23 00:31:46 +0200

    myfrist commit

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'users',
     'pages',
+    'articles',
     'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -108,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Cairo'
 
 USE_I18N = True
 
